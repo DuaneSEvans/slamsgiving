f0806d38bb712f59a6f4faf4a03acf698cb137a5
Duane Evans <duanesevans@gmail.com>
2023-04-08 15:59:48 -0700

    create the world

---
+++ pages/api/hello.ts
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
+
+export default function handler(_req: NextRequest, res: NextApiResponse) {
+  res.status(200).json({ name: "John Doe" });
+}
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="text-center p-8">
+      <h1 className="text-6xl font-bold">SLAMSGIVING</h1>
+      <p className="mt-auto">Coming soon</p>
+    </div>
+  );
+}
