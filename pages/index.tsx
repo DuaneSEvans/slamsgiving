5d2b7d455cf55f67b8de84811ac021a5dbcf754d
Duane Evans <duanesevans@gmail.com>
2023-04-08 22:23:42 -0700

    fix a few things

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div className="h-screen flex flex-col justify-between text-center p-12">
+    <div className="flex flex-col justify-between text-center p-12">
       <h1 className="md:text-6xl text-4xl font-bold">SLAMSGIVING</h1>
-      <p>Coming soon</p>
+      <p className="">Coming soon</p>
     </div>
   );
 }
