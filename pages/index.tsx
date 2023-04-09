ff7c0d047f9f21cd9cb30529223e6cadd201a27e
Duane Evans <duanesevans@gmail.com>
2023-04-08 22:25:18 -0700

    add a touch of padding

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex flex-col justify-between text-center p-12">
       <h1 className="md:text-6xl text-4xl font-bold">SLAMSGIVING</h1>
-      <p className="">Coming soon</p>
+      <p className="p-6">Coming soon</p>
     </div>
   );
 }
