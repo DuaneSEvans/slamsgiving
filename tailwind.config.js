1796890a1948c3d5cbb0c1af6fdc301bf3f5ce6b
Duane Evans <duanesevans@gmail.com>
2023-04-08 21:19:30 -0700

    fix typo

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     "./app/**/*.{html,js,ts,jsx,tsx}",
     "./pages/**/*.{html,js,ts,jsx,tsx}",
-    "./components/**/*.{htmljs,ts,jsx,tsx}",
+    "./components/**/*.{html,js,ts,jsx,tsx}",
 
     // Or if using `src` directory:
     "./src/**/*.{html,js,ts,jsx,tsx}",
