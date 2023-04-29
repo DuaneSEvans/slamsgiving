08764084e40e73f9406e1c4aafeafa51ed74e3d0
Duane Evans <duanesevans@gmail.com>
2023-04-29 09:36:25 -0700

    will this break everything

---
+++ components/CountDown.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import { DateTime, Duration } from "luxon";
+
+const SLAMSGIVING_DATE = DateTime.local(2023, 11, 25, 16, 20, 0).setZone(
+  "America/Vancouver"
+);
+
+function getTimeUntilSlams(): Duration {
+  return SLAMSGIVING_DATE.diff(DateTime.now(), [
+    "months",
+    "days",
+    "hours",
+    "minutes",
+    "seconds",
+    "milliseconds",
+  ]);
+}
+
+export default function CountDown() {
+  const [timeRemaining, setTimeRemaining] = useState(getTimeUntilSlams());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeRemaining(getTimeUntilSlams());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <>
+      <div className="flex justify-center lg:gap-6 gap-4">
+        <CountDownBox value={timeRemaining.months} label="months" />{" "}
+        <CountDownBox value={timeRemaining.days} label="days" />{" "}
+        <CountDownBox value={timeRemaining.hours} label="hours" />{" "}
+        <CountDownBox value={timeRemaining.minutes} label="minutes" />{" "}
+        <CountDownBox value={timeRemaining.seconds} label="seconds" />{" "}
+      </div>
+      <article className="p-6">
+        <p>{SLAMSGIVING_DATE.toFormat("MMMM dd, yyyy")}</p>
+        <p>{SLAMSGIVING_DATE.toFormat("h:mm a")}</p>
+      </article>
+    </>
+  );
+}
+
+function CountDownBox({
+  value,
+  label,
+}: {
+  value: number;
+  label: string;
+}): JSX.Element {
+  return (
+    <div className="flex flex-col lg:p-5 p-2">
+      <span className="lg:text-8xl md:text-5xl text-3xl">{value}</span>
+      <span>{label}</span>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
+import CountDown from "../components/countdown";
+
 export default function Home() {
   return (
     <div className="h-full flex flex-col justify-between text-center p-12">
       <h1 className="md:text-6xl text-4xl font-bold uppercase">slamsgiving</h1>
-      <p className="p-6">Coming soon</p>
+      <CountDown />
     </div>
   );
 }
