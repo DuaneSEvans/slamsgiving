93f70577e55580d553b29d195af0698065302aac
Duane Evans <duanesevans@gmail.com>
2023-04-29 17:13:08 -0700

    little more breathing room

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { CountDown } from "../components/CountDown";
 
 export default function Home() {
   return (
-    <div className="h-full flex flex-col justify-between text-center p-12">
+    <div className="h-full flex flex-col justify-between text-center py-12 md:px-12">
       <h1 className="md:text-6xl text-4xl font-bold uppercase">slamsgiving</h1>
       <CountDown />
     </div>
