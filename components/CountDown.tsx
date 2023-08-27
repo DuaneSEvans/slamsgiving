d98c105ddd279429c6afe035d8495ac42db5bb63
Duane Evans <duanesevans@gmail.com>
2023-08-26 20:06:52 -0700

    update date to nov 16

--- components/CountDown.tsx
+++ components/CountDown.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { DateTime, Duration } from "luxon";
 
-const SLAMSGIVING_DATE = DateTime.local(2023, 11, 25, 16, 20, 0).setZone(
+const SLAMSGIVING_DATE = DateTime.local(2023, 11, 18, 16, 20, 0).setZone(
   "America/Vancouver"
 );
 
