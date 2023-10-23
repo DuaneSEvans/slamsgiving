ff88384e6e520a207e1eb905a3b675f4a9910eba
Duane Evans <duanesevans@gmail.com>
2023-10-22 19:35:16 -0700

    tweaks

--- components/EventData.tsx
+++ components/EventData.tsx
@@ -2,7 +2,7 @@ import { SLAMSGIVING_DATE } from "lib/const"
 
 export function EventDate(): JSX.Element {
   return (
-    <article className="p-6">
+    <article className="p-6 cursive-font font-bold md:text-2xl text-lg">
       <p>{SLAMSGIVING_DATE.toFormat("MMMM dd, yyyy")}</p>
       <p>{SLAMSGIVING_DATE.toFormat("h.mm a")}</p>
     </article>
