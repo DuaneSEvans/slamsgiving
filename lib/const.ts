0dc4435c7c53b4dcca84bcd9b880021f0bc7e685
Duane Evans <duanesevans@gmail.com>
2023-11-20 21:04:38 -0800

    Slams 2024

--- lib/const.ts
+++ lib/const.ts
@@ -1,5 +1,5 @@
 import { DateTime } from "luxon"
 
-export const SLAMSGIVING_DATE = DateTime.local(2023, 11, 18, 16, 20, 0).setZone(
+export const SLAMSGIVING_DATE = DateTime.local(2024, 11, 23, 16, 20, 0).setZone(
   "America/Vancouver"
 )
