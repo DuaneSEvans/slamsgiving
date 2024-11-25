d05e5930017894930396b4f8298362a26ae17458
Duane Evans <duanesevans@gmail.com>
2024-11-24 20:33:27 -0800

    no more tickets

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           </p>
         </article>
       </div>
-      <TicketButton />
+      {/* <TicketButton /> */}
       <CountDown />
       <EventDate />
     </div>
