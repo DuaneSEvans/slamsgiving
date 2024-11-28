d44e03fd375b36fa09e233969ec81ee28c0199b3
Duane Evans <duanesevans@gmail.com>
2024-11-27 19:32:09 -0800

    add 2023 gallery

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,18 +3,56 @@ import Link from "next/link"
 
 export default function Nav(): JSX.Element {
   return (
-    <nav className="bg-primary flex p-2 justify-between">
+    <nav className="bg-primary p-2 justify-between flex">
       <Link href="/" className="p-1">
         <Image src="/shrek-head.svg" alt="Home" width="32" height="32" />
       </Link>
-      <div className="flex gap-12">
-        <Link href="/what" className="flex items-center mr-1 text-swamp">
-          What
-        </Link>
-        <Link href="/about-us" className="flex items-center mr-1 text-swamp">
-          About us
-        </Link>
-      </div>
+      <DesktopNav />
     </nav>
+  )
+}
+
+
+function DesktopNav(): JSX.Element {
+  return (
+    <div className="flex gap-6">
+      <NavMenuOptions />
+    </div>
+  )
+}
+
+function NavMenuOptions({ onClick }: { onClick?: () => void }): JSX.Element {
+  return (
+    <>
+      <NavMenuOption href="/gallery" onClick={onClick}>
+        Gallery
+      </NavMenuOption>
+      <NavMenuOption href="/what" onClick={onClick}>
+        What
+      </NavMenuOption>
+      <NavMenuOption href="/about-us" onClick={onClick}>
+        About us
+      </NavMenuOption>
+    </>
+  )
+}
+
+function NavMenuOption({
+  href,
+  children,
+  onClick,
+}: {
+  href: string
+  children: React.ReactNode
+  onClick?: () => void
+}): JSX.Element {
+  return (
+    <Link
+      href={href}
+      className="flex items-center mr-1 text-swamp"
+      onClick={onClick}
+    >
+      {children}
+    </Link>
   )
 }
---
+++ pages/gallery.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable @next/next/no-img-element */
+export default function Gallery(): JSX.Element {
+  return (
+    <div className="w-full flex justify-center">
+      <div className="flex flex-col w-full items-center gap-16 h-full text-swamp py-12 px-2 md:px-12">
+        <div className="flex flex-col gap-6">
+          <h1 className="text-center text-2xl sm:text-4xl font-bold">2024</h1>
+          <br />
+          <article className="flex justify-center text-justify">
+            Still cooking 🍳
+          </article>
+          <br />
+          <br />
+          <h1 className="text-center text-2xl sm:text-4xl font-bold">2023</h1>
+          <article className="flex justify-center text-justify">
+            <Mosaic />
+          </article>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function Mosaic(): JSX.Element {
+  const images = Array.from({ length: 76 }, (_, i) => `2023/${i + 1}.jpg`)
+
+  return (
+    <ul className="columns-1 md:columns-2 lg:columns-3 gap-4 p-4">
+      {images.map((src) => (
+        <li className="break-inside-avoid" key={src}>
+          <img className="block w-full mb-4" src={src} alt="🍳" />
+        </li>
+      ))}
+    </ul>
+  )
+}
