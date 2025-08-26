525e45fb0cff1bed280fa1dc8f816d3684fe0836
DuaneSEvans <79600154+DuaneSEvans@users.noreply.github.com>
2025-08-26 09:13:41 +0900

    Phone commit 2

--- components/TicketButton.tsx
+++ components/TicketButton.tsx
@@ -4,7 +4,7 @@ export function TicketButton(): JSX.Element {
   return (
     <div className="flex justify-center">
       <Link
-        href="https://forms.gle/YZrAVThivf2kf4E78"
+        href="https://forms.gle/CdqVGwFkcyP7U4HJ8"
         target="_blank"
         className="text-2xl bg-primary font-semibold border-transparent focus:ring-blue-500 shadow-md py-2 px-4 rounded-lg hover:scale-105 transition-all"
       >
